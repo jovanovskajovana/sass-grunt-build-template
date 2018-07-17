3221d15cfdec6433081dfec6330661e4cca0cadc
Jovana Jovanovska <jovanovska.jovana1@gmail.com>
2018-07-17 17:33:41 +0200

    Add grunt tasks

---
+++ gruntfile.js
@@ -0,0 +1,110 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      js: {
+        files: ['src/js/*.js'],
+        tasks: ['uglify:dev']
+      },
+      css: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass:dev']
+      },
+      view: {
+        files: ['src/view/*.html'],
+        tasks: ['copy']
+      }
+    },
+    uglify: {
+      dev: {
+        options: {
+          beautify: true,
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        files: [{
+          expand: true,
+          cwd  :'src/js/',
+          src  :'*.js',
+          dest :'dist/js',
+          ext  :'.min.js'
+        }]
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd  :'src/js/',
+          src  :'*.js',
+          dest :'dist/js',
+          ext  :'.min.js'
+        }]
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: [{
+          expand: true,
+          cwd  :'src/scss/',
+          src  :'*.scss',
+          dest :'dist/css',
+          ext  :'.min.css'
+        }]
+      },
+      build: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: [{
+          expand: true,
+          cwd  :'src/scss/',
+          src  :'*.scss',
+          dest :'dist/css',
+          ext  :'.min.css'
+        }]
+      }
+    },
+    copy: {
+      'view': {
+        files: [{
+          expand: true,
+          cwd: 'src/view',
+          src: '**/*',
+          dest: 'dist',
+          filter: 'isFile',
+          flatten: true,
+        }]
+      },
+      'img': {
+        files: [{
+          expand: true,
+          cwd: 'src/img',
+          src: '**/*',
+          dest: 'dist/img',
+          filter: 'isFile',
+          flatten: true,
+        }]
+      }
+    },
+    connect: {
+      dev: {
+        port: 8000,
+        base: '.'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['uglify:dev','sass:dev','connect:dev','copy','watch']);
+  grunt.registerTask('build', ['uglify:build', 'sass:build','copy']);
+
+};
